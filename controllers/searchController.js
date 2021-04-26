13aff4c53701f0dc115f70d9a0bfae40bd02235c
martin albores <malbores@udesa.edu.ar>
2021-04-26 16:18:12 -0300

    ruta-controlador-vista de buscador (me falta mejorar)

---
+++ controllers/searchController.js
@@ -0,0 +1,17 @@
+const productDescription = require('../product-description');
+
+const search = {
+    detail: (req,res) => {
+        const productId = req.params.id;
+        for (let i = 0; i < productDescription.length; i++) {
+            const element = productDescription[i];
+            if (productId == element.id) {
+                return res.render('product', {product: element});
+            }
+        }
+        
+    },
+
+};
+
+module.exports = search;
